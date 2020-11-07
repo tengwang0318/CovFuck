abd2d3c8810666c1b225ac5892b0f5643f031f7a
wangtengxoxo <wt0318@outlook.com>
2020-11-07 14:22:45 +0800

    Add files via upload

---
+++ CovFuck.py
@@ -0,0 +1,16 @@
+import time
+from selenium import webdriver
+browser = webdriver.Firefox()
+browser.get("http://authserver.nwu.edu.cn/authserver/login?service=https%3A%2F%2Fapp.nwu.edu.cn%2Fa_nwu%2Fapi%2Fsso%2Fcas%3Fredirect%3Dhttps%253A%252F%252Fapp.nwu.edu.cn%252Fsite%252Fncov%252Fdailyup%26from%3Dwap")
+account_field = browser.find_element_by_name("username")
+password_filed = browser.find_element_by_id('password')
+account_field.send_keys('2018103399')
+password_filed.send_keys('0000318@.wt')
+browser.find_element_by_xpath('/html/body/div[3]/div[2]/div[2]/div/div[2]/div[1]/form/p[6]/button').click()
+time.sleep(10)
+browser.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[4]/div/input').click()
+time.sleep(10)
+browser.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[5]').click()
+time.sleep(10)
+browser.find_element_by_xpath('/html/body/div[5]/div/div[2]/div[2]').click()
+browser.quit()
