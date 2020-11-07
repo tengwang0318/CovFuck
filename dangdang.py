c8ed6e874c5ae0bbc0fb9eaf6b947574fde802ab
wangtengxoxo <wt0318@outlook.com>
2020-11-07 15:16:14 +0800

    Add files via upload

---
+++ dangdang.py
@@ -0,0 +1,50 @@
+import requests
+import re
+import json
+
+
+def main(page):
+    url = 'http://bang.dangdang.com/books/fivestars/01.00.00.00.00.00-recent30-0-0-1-' + str(page)
+    html = request_web(url)
+    items = parse_result(html)
+
+    for item in items:
+        write_item_to_file(item)
+        print(item)
+
+
+def request_web(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+    except requests.RequestException:
+        return None
+
+
+def parse_result(html):
+    pattern = re.compile(
+        '<li>.*?list_num.*?(\d+).</div>.*?<img src="(.*?)".*?class="name".*?title="(.*?)">.*?class="star">.*?class="tuijian">(.*?)</span>.*?class="publisher_info">.*?target="_blank">(.*?)</a>.*?class="biaosheng">.*?<span>(.*?)</span></div>.*?<p><span\sclass="price_n">&yen;(.*?)</span>.*?</li>',
+        re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'range': item[0],
+            'iamge': item[1],
+            'title': item[2],
+            'recommend': item[3],
+            'author': item[4],
+            'times': item[5],
+            'price': item[6]
+        }
+
+
+def write_item_to_file(items):
+    print("It's time for web crawler!")
+    with open('book.txt', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(items, ensure_ascii=False) + "\n")
+
+
+if __name__ == "__main__":
+    for i in range(1, 26):
+        main(i)
